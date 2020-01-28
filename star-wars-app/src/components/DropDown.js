65322af4e99a4d64bd234d7b4036e81a4967d05b
Jose <Jrivera6869@yahoo.com>
2020-01-27 20:21:40 -0500

    Stretch

--- star-wars-app/src/components/DropDown.js
+++ star-wars-app/src/components/DropDown.js
@@ -1,13 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from "reactstrap";
+
+import { useDispatch } from "react-redux";
 
 const DropDown = () => {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const dispatch = useDispatch();
+
+  const toggle = () => setDropdownOpen(prevState => !prevState);
+
   return (
     <div>
-      <p>People</p>
-      <p>Planets</p>
-      <p>StarShips</p>
-      <p>Vehicles</p>
-      <p>Species</p>
+      <Dropdown className='drop-top' isOpen={dropdownOpen} toggle={toggle}>
+        <DropdownToggle style={{ fontWeight: "bolder" }} caret>
+          Dropdown
+        </DropdownToggle>
+        <DropdownMenu>
+          <DropdownItem header>Selections</DropdownItem>
+          <DropdownItem onClick={() => dispatch({ type: "PEOPLE" })}>
+            People
+          </DropdownItem>
+          <DropdownItem onClick={() => dispatch({ type: "PLANETS" })}>
+            Planets
+          </DropdownItem>
+          <DropdownItem onClick={() => dispatch({ type: "STARSHIPS" })}>
+            Starships
+          </DropdownItem>
+          <DropdownItem onClick={() => dispatch({ type: "VEHICLES" })}>
+            Vehicles
+          </DropdownItem>
+          <DropdownItem onClick={() => dispatch({ type: "SPECIES" })}>
+            Species
+          </DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
     </div>
   );
 };
--- star-wars-app/src/components/StarWarsCharacters.test.js
+++ star-wars-app/src/components/StarWarsCharacters.test.js
@@ -2,15 +2,29 @@ import React from "react";
 import { render, fireEvent, wait } from "@testing-library/react";
 import StarWarsCharacters from "./StarWarsCharacters";
 import { getData as mockGetData } from "../api";
-import { shallow } from "enzyme";
-import axios from "axios";
+import { Provider } from "react-redux";
+import Enzyme, { mount } from 'enzyme';
+import { reducer } from "../reducers";
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+import { createStore } from 'redux';
 
-jest.mock("../api");
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+describe('<StarWarsCharacters /> unit test', () => {
+    const getWrapper = (mockStore = createStore(reducer, { url: "https://swapi.co/api/people" })) => mount(
+        <Provider store={mockStore}>
+      <StarWarsCharacters/>
+    </Provider>
+  );
+  
+  
+  jest.mock("../api");
+
 
 test("render the Starwars names correctly", async () => {
   //   mockGetData.mockReturnValueOnce(true);
 
-  const { getByText } = render(<StarWarsCharacters />);
+  const { getByText } = getWrapper();
 
   getByText(/Previous/i);
   getByText(/next/i);
@@ -27,9 +41,9 @@ test("testing api call", () => {
   expect(mockGetData).toHaveBeenCalledTimes(1);
 });
 
-test("is button firing?", () => {
-  const { getByText } = render(<StarWarsCharacters />);
+// test("is button firing?", () => {
+//   const { getByText } = render(<StarWarsCharacters />);
 
-  const nextButton = getByText(/next/i);
-  fireEvent.click(nextButton);
-});
+//   const nextButton = getByText(/next/i);
+//   fireEvent.click(nextButton);
+// })
